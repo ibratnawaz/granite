9eef94d34e9b7c196e15560d6fdb25341a8e8ba3
Ibrat Nawaz <ibratrocker99@gmail.com>
2021-04-22 12:00:52 +0530

    Added reusable components

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 const App = () => {
   useEffect(() => {
-    /*eslint no-undef: "off"*/
     initializeLogger();
     logger.info("Log from js-logger");
   }, []);
